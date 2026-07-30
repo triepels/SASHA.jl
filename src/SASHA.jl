ea8832498ec7143ba9b59a72a481579fd3604a94
Ron <>
2026-07-30 15:56:29 +0200

    Improved docstrings.

--- src/SASHA.jl
+++ src/SASHA.jl
@@ -51,21 +51,21 @@ end
 """
     space(; iters...)
 
-    Returns an iterator over the product of several named iterators. Each generated
-    element is a named tuple whose ith element is taken from the ith iterator. The first
-    iterator changes the fastest.
+Returns an iterator over the product of several named iterators. Each generated
+element is a named tuple whose ith element is taken from the ith iterator. The first
+iterator changes the fastest.
 
-    # Examples
-    ```jldoctest
-    julia> collect(space(a=1:3, b=4:5))
-    6-element Vector{@NamedTuple{a::Int64, b::Int64}}:
-    (a = 1, b = 4)
-    (a = 2, b = 4)
-    (a = 3, b = 4)
-    (a = 1, b = 5)
-    (a = 2, b = 5)
-    (a = 3, b = 5)
-    ```
+# Examples
+```jldoctest
+julia> collect(space(a=1:3, b=4:5))
+6-element Vector{@NamedTuple{a::Int64, b::Int64}}:
+(a = 1, b = 4)
+(a = 2, b = 4)
+(a = 3, b = 4)
+(a = 1, b = 5)
+(a = 2, b = 5)
+(a = 3, b = 5)
+```
 """
 space(; vars...) = space(keys(vars), values(values(vars)))
 space(names::NTuple{N, Symbol}, vars::NTuple{N, Any}) where N = Space{names, typeof(vars)}(vars)
@@ -76,11 +76,11 @@ _fit!(model::Any, data::Any, args::NamedTuple) = fit!(model, data; args...)
 """
     fit!(model, data; <keyword arguments)
 
-    Fits `model` on `data` based on any additional provided keyword arguments and returns
-    the fitted model. Should be implemented for any custom model type before using the
-    SASHA optimization functions.
+Fits `model` on `data` based on any additional provided keyword arguments and returns
+the fitted model. Must be implemented for any custom model type before using the SASHA
+optimization functions.
 
-    See also [`sasha`](@ref), [`sasha!`](@ref).
+See also [`sasha`](@ref), [`sasha!`](@ref).
 """
 fit!(model::Any, data::Any; args::NamedTuple) = throw(MethodError(fit!, (model, data, args)))
 
@@ -90,39 +90,39 @@ _loss(model::Any, data::Any) = loss(model, data)
 """
     loss(model, data)
 
-    Evaluates the loss of `model` on `data`. Should be implemented for any custom model
-    type before using the SASHA optimization functions.
+Evaluates the loss of `model` on `data`. Must be implemented for any custom model type
+before using the SASHA optimization functions.
 
-    See also [`sasha`](@ref), [`sasha!`](@ref).
+See also [`sasha`](@ref), [`sasha!`](@ref).
 """
 loss(model::Any, data::Any) = throw(MethodError(loss, (model, data)))
 
 """
     sasha!([rng=GLOBAL_RNG], arms, train, val; <keyword arguments>)
 
-    Performs hyperparameter optimization using the SASHA optimizer on `arms` by modifying
-    the vector of arms in-place. Each arm is fitted on `train` by iteratively calling
-    fit!(`model`, `train`; `args`...). The loss of each arm is evaluated on `val` by
-    calling loss(`model`, `val`). These functions must be implemented for the type of the
-    model to be optimized. If `train` or `val` are (named) tuples, the arguments are
-    splatted before the respective function calls. Returns a tuple with the winning arm
-    and its final loss.
-    
-    Multiple threads will be used (if available) to fit all remaining arms each round. The
-    number of available threads can be determined by calling Threads.nthreads().
+Performs hyperparameter optimization using the SASHA optimizer on `arms` by modifying
+the vector of arms in-place. Each arm is fitted on `train` by iteratively calling
+`fit!`(`model`, `train`; `args`...). The loss of each arm is evaluated on `val` by
+calling `loss`(`model`, `val`). These functions must be implemented for the type of the
+model to be optimized. If `train` or `val` are (named) tuples, the arguments are
+splatted in the respective function calls. Returns a tuple with the winning arm and its
+final loss.
 
-    # Keyword Arguments
-    - `p::Real=0.8`: average acceptance probability of the arms at the start of the
-    optimization (i.e., before any arms are fitted).
-    - `maximize::Bool=false`: whether the loss of the arms should be maximized.
-    - `args::NamedTuple=NamedTuple())`: optional named arguments that are passed on to
-    [`fit!`](@ref) when fitting an arm.
+Multiple threads will be used (if available) to fit all remaining arms each round. The
+number of available threads can be determined by calling Threads.nthreads().
 
-    # References
-    Triepels, R. (2023). SASHA: Hyperparameter Optimization by Simulated Annealing and
-    Successive Halving.
+# Keyword Arguments
+- `p::Real=0.8`: average acceptance probability of the arms at the start of the
+  optimization (i.e., before any arms are fitted).
+- `maximize::Bool=false`: whether the loss of the arms should be maximized.
+- `args::NamedTuple=NamedTuple())`: optional named arguments that are passed on to
+  `fit!` when fitting an arm.
 
-    See also [`sasha`](@ref)
+# References
+Triepels, R. (2023). SASHA: Hyperparameter Optimization by Simulated Annealing and
+Successive Halving.
+
+See also [`sasha`](@ref)
 """
 function sasha!(rng::AbstractRNG, arms::Vector, train::Any, val::Any; p::Real=0.8,
     maximize::Bool=false, args::NamedTuple=NamedTuple())
@@ -173,33 +173,33 @@ sasha!(arms::Vector, train::Any, val::Any; p::Real=0.8, maximize::Bool=false,
     sasha([rng=GLOBAL_RNG], T, space, train, val; <keyword arguments>)
     sasha([rng=GLOBAL_RNG], f, space, train, val; <keyword arguments>)
 
-    Optimizes the hyperparameters of a model using the SASHA optimizer. Initially, an arm 
-    is created for each configuration in `space` by calling the constructor of type `T`
-    with model parameters provided as named arguments. Alternatively, if `T` cannot have a
-    constructor with named arguments, function `f` can be provided to create each arm
-    instead. `f` should take a named tuple with model parameters as input and initialize a
-    new model based on the provided parameter configuration. Each arm is fitted on `train`
-    by iteratively calling fit!(`model`, `train`; `args`...). The loss of each arm is
-    evaluated on `val` by calling loss(`model`, `val`). These functions must be
-    implemented for the type of the model to be optimized. If `train` or `val` are (named) 
-    tuples, the arguments are splatted before the respective function calls. Returns a
-    tuple with the winning arm and its final loss.
-    
-    Multiple threads will be used (if available) to fit all remaining arms each round. The
-    number of available threads can be determined by calling Threads.nthreads().
+Optimizes the hyperparameters of a model using the SASHA optimizer. Initially, an arm
+is created for each configuration in `space` by calling the constructor of type `T`
+with model parameters provided as named arguments. Alternatively, if `T` cannot have a
+constructor with named arguments, function `f` can be provided to create each arm
+instead. `f` should take a named tuple with model parameters as input and initialize a
+new model based on the provided parameter configuration. Each arm is fitted on `train`
+by iteratively calling `fit`!(`model`, `train`; `args`...). The loss of each arm is
+evaluated on `val` by calling `loss`(`model`, `val`). These functions must be
+implemented for the type of the model to be optimized. If `train` or `val` are (named)
+tuples, the arguments are splatted in the respective function calls. Returns a tuple with
+the winning arm and its final loss.
 
-    # Keyword Arguments
-    - `p::Real=0.8`: average acceptance probability of the arms at the start of the
-    optimization (i.e., before any arms are fitted).
-    - `maximize::Bool=false`: whether the loss of the arms should be maximized.
-    - `args::NamedTuple=NamedTuple())`: optional named arguments that are passed on to
-    [`fit!`](@ref) when fitting an arm.
+Multiple threads will be used (if available) to fit all remaining arms each round. The
+number of available threads can be determined by calling Threads.nthreads().
 
-    # References
-    Triepels, R. (2023). SASHA: Hyperparameter Optimization by Simulated Annealing and
-    Successive Halving.
+# Keyword Arguments
+- `p::Real=0.8`: average acceptance probability of the arms at the start of the
+  optimization (i.e., before any arms are fitted).
+- `maximize::Bool=false`: whether the loss of the arms should be maximized.
+- `args::NamedTuple=NamedTuple())`: optional named arguments that are passed on to
+  `fit!` when fitting an arm.
 
-    See also [`sasha!`](@ref)
+# References
+Triepels, R. (2023). SASHA: Hyperparameter Optimization by Simulated Annealing and
+Successive Halving.
+
+See also [`sasha!`](@ref)
 """
 function sasha(rng::AbstractRNG, T::Type, space::Union{Space, Vector{V}}, train::Any,
     val::Any; p::Real=0.8, maximize::Bool=false, args::NamedTuple=NamedTuple()) where V<:NamedTuple
