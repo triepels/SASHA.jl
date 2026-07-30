8af8c24fba54a446f58484f225c06e99e6f9243e
Ron <>
2026-07-30 16:40:23 +0200

    Improved tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using SASHA
 import SASHA: fit!, loss
 
 mutable struct MyModel
-    const β::Float64
+    const η::Float64
     n::Int
-    MyModel(; β::Float64) = new(β, 0)
+    MyModel(; η::Float64) = new(η, 0)
 end
 
 function fit!(model::MyModel, x::AbstractArray; epochs::Int = 1)
@@ -14,15 +14,15 @@ function fit!(model::MyModel, x::AbstractArray; epochs::Int = 1)
 end
 
 function loss(model::MyModel, x::AbstractArray)
-    return -model.β * log(model.n / 100 + 1e-4)
+    return model.η + (1 - model.η)^(model.n + 1) + 1e-3 * rand()
 end
 
 train = rand(2, 10)
 val = rand(2, 10)
 
-sp = space(β=0.05:0.05:1.0)
+sp = space(η=logrange(1e-3, 1e-1, length=20))
 
-sasha(MyModel, sp, train, val, p=0.8, maximize=false, args=(epochs=10,))
-sasha(MyModel, rand(sp, 10), train, val, p=0.8, maximize=false, args=(epochs=10,))
-sasha((args) -> MyModel(; args...), sp, train, val, p=0.8, maximize=false, args=(epochs=10,))
-sasha((args) -> MyModel(; args...), rand(sp, 10), train, val, p=0.8, maximize=false, args=(epochs=10,))
+sasha(MyModel, sp, train, val, p=0.8, maximize=false, args=(epochs=100,))
+sasha(MyModel, rand(sp, 10), train, val, p=0.8, maximize=false, args=(epochs=100,))
+sasha((args) -> MyModel(; args...), sp, train, val, p=0.8, maximize=false, args=(epochs=100,))
+sasha((args) -> MyModel(; args...), rand(sp, 10), train, val, p=0.8, maximize=false, args=(epochs=100,))
